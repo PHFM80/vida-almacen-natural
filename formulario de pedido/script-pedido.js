8e7c7a7c3d3750895d8dd975b0279fe0a4394a1e
Pablo Flores <pablofloresmaza@gmail.com>
2022-03-18 12:09:18 -0300

    version 1

---
+++ formulario de pedido/script-pedido.js
@@ -0,0 +1,5 @@
+function showUbicacion(){
+    document.getElementById(`ubicacion`).style.display="";
+    document.getElementById(`div2`).style.display="none";
+    document.getElementById(`div3`).style.display="none";
+}
